fb080ae9a364aecd45f11c2e61e841dfd4356fbd
Harry <jcceong@gmail.com>
2019-01-29 11:42:06 -0800

    displaying all registers on to canvas

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 let chip8 = new Chip8();
-let Memcanvas = document.getElementById("Memory");
-let reg1 = Memcanvas.getContext("2d");
-reg1.font = "14px Arial";
-reg1.fillStyle = "white";
-reg1.fillText("V0 = " + chip8.getV(0).toString(),0,14);
+let regArr = new Array(16);
+
+let i = 0
+for (let r = 1; r <= 2; r++) {
+    for (i; i < 11*r-((r-1)*3); i++) {
+        //console.log(i,chip8.getV(i),(r-1)*30,((i-11*(r-1))+1)*13)
+        let msg = "V"+i.toString()+"= "+chip8.getV(i).toString();
+        regArr[i] = new textObj(document.getElementById("Memory"), msg, (r-1)*100, ((i-11*(r-1))+1)*14);
+        regArr[i].printToCanvas();
+    }
+}
--- scripts/render.js
+++ scripts/render.js
@@ -1,7 +1,27 @@
-function printToCanvas(canvasObj, msg, x, y) {
-    let temp = canvasObj.getContext("2d");
-    temp.font = "12px Arial";
+function textObj(canvasObj, msg, x, y) {
+    this.x = x;
+    this.y = y;
+    this.canvas = canvasObj;
+    this.msg = msg;
+}
+
+textObj.prototype.printToCanvas = function () {
+    let temp = this.canvas.getContext("2d");
+    temp.font = "13px Arial";
     temp.fillStyle = "white";
-    temp.fillText(msg, x,y);
+    temp.fillText(this.msg, this.x, this.y);
+}
+
+textObj.prototype.setXY = function (x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+textObj.prototype.setCanvas = function (canvasObj) {
+    this.canvas = canvasObj;
+}
+
+textObj.prototype.setMsg = function (msg) {
+    this.msg = msg;
 }
 
